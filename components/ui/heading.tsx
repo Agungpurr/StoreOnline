51a4dcf50bb79f9da052ebf62a029c08a4f77313
Agungpurr <agungpur1102@gmail.com>
2025-06-13 11:24:33 +0700

    seperator

---
+++ components/ui/heading.tsx
@@ -0,0 +1,13 @@
+interface HeadingProps {
+  title: string;
+  description: string;
+}
+
+export const Heading: React.FC<HeadingProps> = (props) => {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold tracking-tight">{props.title}</h2>
+      <p className="text-sm text-muted-foreground">{props.description}</p>
+    </div>
+  );
+};
